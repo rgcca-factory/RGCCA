8a81d555d5b4a4f0bbc03de44419138a5a2980be
cpeltier <caroline.peltier@icm-institute.org>
2019-07-18 11:35:19 +0200

    Adding simulationNA in test files

--- Test/imputeSBTest.R
+++ Test/imputeSBTest.R
@@ -2,7 +2,6 @@
 # 
 # '''
 #setwd("/home/caroline.peltier/Bureau/RGCCA")
-rm(list=ls())
 library(RGCCA)
 library(MASS)
 library(nipals)
@@ -23,19 +22,3 @@ A = list(agri=X_agric, ind=X_ind, polit=X_polit)
 testDataSB=imputeSB(A=A,ncomp=rep(1,3),scale=TRUE,sameBlockWeight=TRUE,tau=rep(1,3),naxis=1,ni=4)
 plot(testDataSB$crit,pch=16)
 plot(testDataSB$stab,pch=16)
-
-
-source("/home/caroline.peltier/Bureau/EtudeNA/readDataset.r")
-repertoire=paste("/home/caroline.peltier/Bureau/EtudeNA/Datasets/Biosca/bloc5/",sep="")
-setwd("/home/caroline.peltier/Bureau/RGCCA")
-namesFiles=dir("./R")
-# loading functions in R directory
-sapply(namesFiles,function(x){source(paste0("./R/",x))})
-
-j=1
-nAxe=2;nBlock=3;scale=TRUE;sameBlockWeight=TRUE,tau=rep(1,3)
- setwd(paste(repertoire,j,sep=""))
-  testData=readDataset(c("CLI","MRS","VOL"))
- resimpute= imputeSB(testData,ncomp=rep(nAxe,nBlock),scale=scale,sameBlockWeight=sameBlockWeight,tau=rep(1,3),tol=1e-8,ni=50,naxis=1)
-  plot(resimpute$crit)
-  plot(resimpute$obj)
--- Test/runAllTest.R
+++ Test/runAllTest.R
@@ -8,8 +8,9 @@ namesFiles=dir("./R")
 sapply(namesFiles,function(x){source(paste0("./R/",x))})
 
 # loading test functions in Test directory
-nameTest=dir("./Test")
-nameTest=nameTest[-which(nameTest=="runAllTest.R")]
+# nameTest=dir("./Test")
+# nameTest=nameTest[-which(nameTest=="runAllTest.R")]
+nameTest=c("cov3Test.R","createNATest.R","defl.selectTest.r","imputeColmeansTest.R","imputeNNTest.R","imputeSBTest.R","initsvdTest.r","intersectionTest.r","MIRGCCATest.R","pmTest.r","rgccakTest.r","rgccaTest.r","scale3Test.R")
 for(i in 1:length(nameTest))
 {
   print(nameTest[i])
