e240fea295827acffaabb8e0056d5d62b84452b3
abourrelier <anatole.bourrelier@icm-institute.org>
2019-07-18 16:44:23 +0200

    Added a test on operating system
    Parallelization fails on windows

--- R/analysis.r
+++ R/analysis.r
@@ -19,8 +19,36 @@ analysis=function(refData,noIntersect=TRUE,C=NULL,tau=NULL,scale=TRUE,nAxe=2,sch
   listFinale=list()
   library(parallel)
   refRgcca=rgcca(refData[1:nBlock],C=C,ncomp=rep(nAxe,nBlock),verbose=FALSE,tau=tau,scale=scale,scheme=scheme,sameBlockWeight=sameBlockWeight,returnA=TRUE)
-
-    listFinale=mclapply(1:nbTestFiles,mc.cores=7,FUN=function(j)
+  
+  if (Sys.info()["sysname"] == "Windows"){
+    listFinale= lapply(1:nbTestFiles, FUN=function(j) {
+      print(paste("dataset",j))
+      setwd(paste0(wd,"/",j))
+      testData=readDataset(bloc = blocknames)
+      listRgcca=list()
+      #	lAxes=lapply(testData,function(m){resPca=PCA(m,graph=FALSE);eigenValues=resPca$eig[,1];nbAxes=critereCoude(eigenValues,graph=TRUE);return(nbAxes)})	
+      #	nbAxesRgcca=as.vector(unlist(lAxes))		
+      # listRgcca[["MI-kNN2"]]=MIRGCCA(testData,k=2,niter=5,scale=scale,sameBlockWeight=TRUE,tau,output="weightedMean",scheme=scheme)$rgcca0
+      listRgcca[["MI-kNNAll"]]=MIRGCCA(testData,k= "all",ni=5,scale=scale,sameBlockWeight=sameBlockWeight,tau=tau,output="mean",scheme=scheme,returnA=TRUE,tol=1e-8)$rgcca0
+      testDataSB=imputeSB(testData,ncomp=rep(nAxe,nBlock),scale=scale,sameBlockWeight=sameBlockWeight,tau=tau,tol=1e-8,ni=10)
+      listRgcca[["EM"]]=rgcca(testDataSB$A,C=C,ncomp=rep(nAxe,nBlock),verbose=FALSE,scale=scale,sameBlockWeight=sameBlockWeight,tau=tau,scheme=scheme,returnA=TRUE,tol=1e-8)
+      listRgcca[["Nipals"]]=rgcca(testData,C=C,ncomp=rep(nAxe,nBlock),verbose=FALSE,na.rm=TRUE,scale=scale,sameBlockWeight=sameBlockWeight,tau=tau,scheme=scheme,returnA=TRUE,tol=1e-8)
+      if(!noIntersect){
+        completeData=intersection(testData)
+        listRgcca[["Complete"]]=rgcca(completeData,C=C,ncomp=rep(nAxe,nBlock),verbose=FALSE,scale=scale,sameBlockWeight=sameBlockWeight,tau=tau,returnA=TRUE,tol=1e-8)
+      }
+      graphics.off()
+      if(noIntersect){selectPatient=NULL}else{selectPatient=rownames(listRgcca[["Complete"]][["Y"]][[1]])}
+      res=lapply(listRgcca, function(x)      {
+        comp=comparison(rgcca1=refRgcca,rgcca2=x,selectPatient=selectPatient);return(comp)
+      })
+      setwd("./..")
+      names(res)=names(listRgcca)
+      return(res)
+    })
+  } else {
+    listFinale= mlcapply(1:nbTestFiles, mc.cores=7,
+                          FUN=function(j)
     {
       print("dataset")
       print(j)
@@ -31,7 +59,7 @@ analysis=function(refData,noIntersect=TRUE,C=NULL,tau=NULL,scale=TRUE,nAxe=2,sch
       #	lAxes=lapply(testData,function(m){resPca=PCA(m,graph=FALSE);eigenValues=resPca$eig[,1];nbAxes=critereCoude(eigenValues,graph=TRUE);return(nbAxes)})	
       #	nbAxesRgcca=as.vector(unlist(lAxes))		
       # listRgcca[["MI-kNN2"]]=MIRGCCA(testData,k=2,niter=5,scale=scale,sameBlockWeight=TRUE,tau,output="weightedMean",scheme=scheme)$rgcca0
-      listRgcca[["MI-kNNAll"]]=MIRGCCA(testData,k="all",ni=5,scale=scale,sameBlockWeight=sameBlockWeight,tau=tau,output="weightedMean",scheme=scheme,returnA=TRUE,tol=1e-8)$rgcca0
+      listRgcca[["MI-kNNAll"]]=MIRGCCA(testData,k= ,ni=5,scale=scale,sameBlockWeight=sameBlockWeight,tau=tau,output="weightedMean",scheme=scheme,returnA=TRUE,tol=1e-8)$rgcca0
       testDataSB=imputeSB(testData,ncomp=rep(nAxe,nBlock),scale=scale,sameBlockWeight=sameBlockWeight,tau=tau,tol=1e-8,ni=10)
       listRgcca[["EM"]]=rgcca(testDataSB$A,C=C,ncomp=rep(nAxe,nBlock),verbose=FALSE,scale=scale,sameBlockWeight=sameBlockWeight,tau=tau,scheme=scheme,returnA=TRUE,tol=1e-8)
       listRgcca[["Nipals"]]=rgcca(testData,C=C,ncomp=rep(nAxe,nBlock),verbose=FALSE,na.rm=TRUE,scale=scale,sameBlockWeight=sameBlockWeight,tau=tau,scheme=scheme,returnA=TRUE,tol=1e-8)
@@ -52,5 +80,6 @@ analysis=function(refData,noIntersect=TRUE,C=NULL,tau=NULL,scale=TRUE,nAxe=2,sch
       names(res)=names(listRgcca)
       return(res)
     })
+  }
   return(listFinale)
 }
