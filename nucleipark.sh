1cc16966951a1dffc01fea486a9e8852d0297658
Etienne Camenen <etienne.camenen@gmail.com>
2019-10-10 10:38:45 +0200

    removed nucleipark files

--- nucleipark.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-for i in $(seq 1 10); do srun --time=1:00:00 --cpus-per-task=4 --partition=normal --mem=2G --job-name=nucleipark --account=bioinfo --mail-user=etienne.camenen@icm-institute.org Rscript R/optspars.R --job-id out/${i} & done
