d50181a82d214f67290cff1862273db2117cc8e1
Arnaud <arnaud.gloaguen@supelec.fr>
2021-03-10 18:40:09 +0100

    Improve the way special cases are dealt with.

--- R/proj_l1_l2.R
+++ R/proj_l1_l2.R
@@ -3,20 +3,49 @@ proj_l1_l2 <- function(argu, a=1){
   norm2_argu = norm2(argu)
   if ( norm2_argu < 1e-32 ) stop("Norm2 of argu is too small :", norm2_argu)
   if ( sum(abs(argu/norm2_argu)) <= a ) return(list(k=NaN, lambda = 0))
+  # The desired a_k cannot be null as the constraints are not already satisfied 
+  # (cf. previous check). So zero values are removed.
   uneq <- argu != 0
-  L <- sum(!uneq)
-  p <- abs(argu[uneq])
+  L    <- sum(!uneq)
+  p    <- abs(argu[uneq])
   #Check if multiple maximum
-  MAX <- max(p)
+  MAX  <- max(p)
   bMAX <- p == MAX
   nMAX <- sum(bMAX)
+  # If there are multiple maximum value, the sparse parameter 
+  # "a" must be >= sqrt(number of max)
   if (a < sqrt(nMAX)){
-    stop("Impossible to project, minimum ratio is : ", sqrt(nMAX))
-  } else if (a == sqrt(nMAX)){
-    warning("a == sqrt(nMAX)")
-    argu_soft        <- rep(0, length(argu))
-    argu_soft[bMAX]  <- 1/sqrt(nMAX)
-    return(list(k=NaN, lambda = MAX - 1/sqrt(nMAX)))
+    warning("Impossible to project, minimum ratio is : sqrt(nMAX) = ", sqrt(nMAX), 
+            ". Hence the sparse parameter is changed for sqrt(nMAX).")
+    a = sqrt(nMAX)
+  } 
+  # If there are multiple maximum value and a = sqrt(number of max), 
+  # solution is straightforward
+  if (a == sqrt(nMAX)){
+    if (length(bMAX) == length(p)){
+      # Case where there is as many maximum as the number of elements of the 
+      # vector to project "argu". Indeed in the case, 
+      # ||argu||_1/||argu||_2 = sqrt(number of max)
+      lambda = 0
+    }else{
+      # With this choice of lambda, the soft-thresholding will set all 
+      # parameters to 0 except for the elements equals to the maximum value.
+      a_2    = max(p[-which(bMAX)])
+      lambda = (MAX + a_2)/2
+    }
+    return(list(k=NaN, lambda = lambda))
+  }
+  # If the vector to project "argu" is composed of 2 elements only, as the 
+  # sparse parameter a <= 1 the desired a_k is a_2 (the lowest element) as 
+  # psi(a_2) = 1 (cf. theory) and by construction a_3 = 0 and psi(0) >= a 
+  # (checked previously). Moreover, these 2 elements are necessarily different 
+  # (cf. conditions above)
+  if (length(p) == 2){
+    a_k     = min(p)
+    psi_a_k = 1
+    k       = 2
+    lambda  = a_k - (a*sqrt((k - psi_a_k^2)/(k - a^2))-psi_a_k)*(sum(p) - k*a_k)/(psi_a_k*(k))
+    return( list(k=NaN, lambda = lambda) )
   }
   #Initialize parameters
   s_1 <- s_2 <- nb <- 0
@@ -66,7 +95,7 @@ proj_l1_l2 <- function(argu, a=1){
       }
     }
   }
-  #Compute lambda and thus the soft tresholded vector to return
+  #Compute lambda
   lambda <- a_k - (a*sqrt((k - psi_a_k^2)/(k - a^2))-psi_a_k)*(s_1 + s_low_1 - k*a_k)/(psi_a_k*(k))
   return( list(k=k, lambda = lambda) )
 }
