95319e8c7b997136ca684818639dadca49c6b6f3
Arnaud <arnaud.gloaguen@supelec.fr>
2021-07-30 13:58:34 +0200

    Documentation and add of 'l2_sat' and 'var_order'.

--- R/group_sgccak.R
+++ R/group_sgccak.R
@@ -1,9 +1,10 @@
-#' The function sgccak() is called by sgcca() and does not have to be used by
-#' the user. sgccak() enables the computation of SGCCA block components, outer
-#' weight vectors, etc., for each block and each deflation stage.
+#' The function group_sgccak() is called by group_sgcca() and does not have to be
+#' used by the user. group_sgccak() enables the computation of group SGCCA block
+#' components, outer weight vectors, etc., for each block and each deflation stage.
 #' @inheritParams select_analysis
 #' @inheritParams rgccad
 #' @inheritParams sgcca
+#' @inheritParams group_sgcca
 #' @inheritParams rgccak
 #' @return \item{Y}{A list of \eqn{J} elements. Each element of \eqn{Y} is a
 #' matrix that contains the analysis components for the corresponding block.}
@@ -23,7 +24,7 @@ group_sgccak <-  function(A, C, sparsity = rep(1, length(A)), group_sparsity, sc
                     scale = FALSE, tol = .Machine$double.eps,
                     init = "svd", bias = TRUE, verbose = TRUE,
                     quiet = FALSE){
-  
+
   J <- length(A)
   pjs = sapply(A, NCOL)
   AVE_X <- rep(0, J)
@@ -40,11 +41,14 @@ group_sgccak <-  function(A, C, sparsity = rep(1, length(A)), group_sparsity, sc
   } else {
     stop_rgcca("init should be either random or svd.")
   }
-  
+
   if (any( sparsity < 1/sqrt(pjs) | sparsity > 1 ))
     stop_rgcca("L1 constraints must vary between 1/sqrt(p_j) and 1.")
-  
-  nb_groups = sapply(group_sparsity, length) 
+
+  nb_groups = sapply(group_sparsity, length)
+  var_order = lapply(group_sparsity, function (x) sort(x            = unlist(x),
+                                                       decreasing   = FALSE,
+                                                       index.return = TRUE*(!is.null(x)))$ix)
   const     = sparsity*(sqrt(pjs)*as.numeric(nb_groups == 0) + sqrt(nb_groups))
   #	Apply the constraints of the general otpimization problem
   #	and compute the outer components
@@ -55,13 +59,14 @@ group_sgccak <-  function(A, C, sparsity = rep(1, length(A)), group_sparsity, sc
     if (is.null(group_sparsity[[q]])){
       a[[q]] <- soft.threshold(a[[q]], const[q])
     }else{
-      a[[q]] <- group_soft.threshold(a[[q]], const[q], group_sparsity = group_sparsity[[q]])
+      a[[q]] <- group_soft.threshold(a[[q]], const[q],
+                                     group_sparsity = group_sparsity[[q]],
+                                     var_order      = var_order[[q]])
     }
-    a[[q]] <- as.vector(a[[q]])/norm2(a[[q]])
     Y[,q]  <- apply(A[[q]],1,miscrossprod,a[[q]])
   }
   a_old <- a
-  
+
   # Compute the value of the objective function
   ifelse((mode(scheme) != "function"),
          {g <- function(x) switch(scheme,
@@ -72,19 +77,19 @@ group_sgccak <-  function(A, C, sparsity = rep(1, length(A)), group_sparsity, sc
          },
          crit_old <- sum(C*scheme(cov2(Y, bias = bias)))
   )
-  
+
   if (mode(scheme) == "function")
     dg = Deriv::Deriv(scheme, env = parent.frame())
-  
+
   repeat{
-    
+
     for (q in 1:J){
-      
+
       if (mode(scheme) == "function") {
         dgx = dg(cov2(Y[, q], Y, bias = bias))
         CbyCovq = C[q, ]*dgx
       }
-      
+
       else{
         if (scheme == "horst")
           CbyCovq <- C[q, ]
@@ -93,18 +98,19 @@ group_sgccak <-  function(A, C, sparsity = rep(1, length(A)), group_sparsity, sc
         if (scheme == "centroid")
           CbyCovq <- C[q, ]*sign(cov2(Y, Y[,q], bias = bias))
       }
-      
+
       Z[,q] <- rowSums(mapply("*", CbyCovq,as.data.frame(Y)))
       a[[q]] <- apply( t(A[[q]]),1,miscrossprod, Z[,q])
       if (is.null(group_sparsity[[q]])){
         a[[q]] <- soft.threshold(a[[q]], const[q])
       }else{
-        a[[q]] <- group_soft.threshold(a[[q]], const[q], group_sparsity = group_sparsity[[q]])
+        a[[q]] <- group_soft.threshold(a[[q]], const[q],
+                                       group_sparsity = group_sparsity[[q]],
+                                       var_order      = var_order[[q]])
       }
-      a[[q]] <- as.vector(a[[q]])/norm2(a[[q]])
       Y[,q] <- apply(A[[q]], 1, miscrossprod,a[[q]])
     }
-    
+
     # check for convergence of the SGCCA algorithm
     ifelse((mode(scheme) != "function"),
            {g <- function(x) switch(scheme,
@@ -115,35 +121,52 @@ group_sgccak <-  function(A, C, sparsity = rep(1, length(A)), group_sparsity, sc
            },
            crit[iter] <- sum(C*scheme(cov2(Y, bias = bias)))
     )
-    
+
     # Print out intermediate fit
-    
+
     if (verbose & (iter %% 1)==0)
       cat(" Iter: ", formatC(iter,width=3, format="d"),
           " Fit: ", formatC(crit[iter], digits=8, width=10, format="f"),
           " Dif: ", formatC(crit[iter]-crit_old, digits=8, width=10, format="f"),
           "\n")
-    
+
     stopping_criteria = c(drop(crossprod(Reduce("c", mapply("-", a, a_old))))
                           , abs(crit[iter]-crit_old))
-    
+
     if ( any(stopping_criteria < tol) | (iter > 1000))
       break
-    
+
     crit_old = crit[iter]
     a_old <- a
     iter <- iter + 1
   }
-  
-  
-  if (iter > 1000) warning("The SGCCA algorithm did not converge after 1000
+
+
+  if (iter > 1000) warning("The group SGCCA algorithm did not converge after 1000
                            iterations.")
-  if(iter<1000 & verbose) cat("The SGCCA algorithm converged to a stationary
+  if(iter<1000 & verbose) cat("The group SGCCA algorithm converged to a stationary
                               point after", iter-1, "iterations \n")
   if (verbose) plot(crit, xlab = "iteration", ylab = "criteria")
-  
+
+  l2_SAT = sapply(a, function(x) norm(x, "2"))
+  if (max(abs(l2_SAT - 1)) > tol){
+    for (i in which(abs(l2_SAT - 1) > tol)){
+      if (l2_SAT[i] < .Machine$double.eps ){
+        warning("Norm2 of the block weight vector #",
+                i, " is too small :", l2_SAT[i])
+      }else{
+        nMAX = length(which(a[[i]] != 0))
+        warning("L2 constraint is not saturated for block #", i, ". The current
+                 value of the sparsity parameter is ", sparsity[i], " and has to
+                be in the range [",
+                sqrt(nMAX/(pjs[i]*as.numeric(nb_groups[i] == 0) + nb_groups[i])),
+                ", 1] in order to saturate the L2 constraint.")
+      }
+    }
+  }
+
   AVE_inner  <- sum(C*cor(Y)^2/2)/(sum(C)/2) # AVE inner model
-  
+
   result <- list(Y = Y, a = a, crit = crit[which(crit != 0)],
                  AVE_inner = AVE_inner)
   return(result)
