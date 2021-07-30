f654b3d733e61e8bd18df6692b89b4ce32c954b5
Arnaud <arnaud.gloaguen@supelec.fr>
2021-07-30 13:59:18 +0200

    Handle 'l2_sat' and thus weights normalization is performed inside the function.

--- R/group_soft.threshold.R
+++ R/group_soft.threshold.R
@@ -5,11 +5,17 @@
 # @return Returns a vector resulting from the soft thresholding of \eqn{x} given sumabs
 # @keywords manip
 #
-group_soft.threshold <- function(x,sumabs=1, group_sparsity){
+group_soft.threshold <- function(x,sumabs=1, group_sparsity, var_order){
   x_sub           = sapply(group_sparsity, function(group) norm(x[group], type = "2"))
-  x_sub_projected = soft(x_sub, proj_l1_l2(x_sub,sumabs)$lambda)
-  x_projected     = lapply(1:length(group_sparsity), function(idx_group) 
+  proj            = proj_l1_l2(argu = x_sub, a = sumabs)
+  x_sub_projected = soft(x = x_sub, d = proj$lambda)
+  x_projected     = lapply(1:length(group_sparsity), function(idx_group)
     as.matrix(x_sub_projected[idx_group]*x[group_sparsity[[idx_group]]]/norm(x[group_sparsity[[idx_group]]], type = "2")))
   x_projected     = Reduce("rbind", x_projected)
-  return(x_projected)
+  x_projected     = x_projected[var_order]
+  if (proj$l2_SAT){
+    return(x_projected/norm(x_projected, type = "2"))
+  }else{
+    return(x_projected)
+  }
 }
