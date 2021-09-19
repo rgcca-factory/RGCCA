6504c292813fb2c8b290a3faebc96d6724d7400e
Arnaud <arnaud.gloaguen@supelec.fr>
2021-09-19 17:52:59 +0200

    Improvment of the prediction part. Now it is performed with the "caret" package --> a lot of prediction models and metrics are thus available !!

--- R/rgcca_predict.R
+++ R/rgcca_predict.R
@@ -4,7 +4,7 @@
 #'
 #' @inheritParams rgcca_transform
 #' @param prediction_model A character giving the function used to compare the trained and the tested models
-#' @param block_to_predict A character or integer giving the block to predict (must be the same name among train and test set)
+#' @param response A character or integer giving the block to predict (must be the same name among train and test set)
 #' @param task A character corresponding to the prediction task among : regression or classification
 #' @examples
 #' data("Russett")
@@ -25,25 +25,24 @@
 #' X = lapply(object1$call$blocks, function(x) x[-c(1:32),])
 #' X = lapply( X, function(x) x[, sample(1:NCOL(x))] )
 #' X = sample(X, length(X))
-#' block_to_predict = "industry"
-#' y_train = kmeans(A[[block_to_predict]], 3)$cluster
-#' y_test = kmeans(X[[block_to_predict]], 3)$cluster
-#' res  = rgcca_predict(object, X, block_to_predict = "industry")
+#' response = "industry"
+#' y_train = kmeans(A[[response]], 3)$cluster
+#' y_test = kmeans(X[[response]], 3)$cluster
+#' res  = rgcca_predict(object, X, response = "industry")
 #' library(MASS)
 #' @importFrom MASS lda
 # @importFrom nnet multinom
 #' @export
 rgcca_predict = function(
   rgcca_res,
-  X,
-  block_to_predict,
-  X_scaled = TRUE,
-  task = "regression",
-  prediction_model = "lm"
+  blocks_test,
+  response,
+  method = "lm",
+  ...
   #   regress_on="block" #TODO
 ) {
   #TODO: Enable to not have the block to predict among the test blocks
-  #TODO: Put back asDisjonctive to enable to project block_to_predict without problems
+  #TODO: Put back asDisjonctive to enable to project response without problems
   ### Auxiliary function
   # Stack projected blocks as a matrix and set colnames as "block_ncomp"
   reformat_projection = function(projection) {
@@ -59,33 +58,30 @@ rgcca_predict = function(
   }
 
   ### Check input parameters
-  match.arg(task, c("regression", "classification"))
-  if (task == "classification" && prediction_model != "lda")
-    stop_rgcca("Only \"lda\" model is available for classification task.")
-  if (task == "regression" && (prediction_model != "lm" && prediction_model != "cor"))
-    stop_rgcca("Only \"cor\" and \"lm\" are available for regression tasks.")
-  if (is.null(names(X))) stop_rgcca("Please provide names for the blocks X.")
+  all_methods = caret::modelLookup()
+  match.arg(method, c("cor", all_methods$model))
+  if (is.null(names(blocks_test))) stop_rgcca("Please provide names for the blocks_test.")
 
-  ### Check that block_to_predict is among both training and test blocks
-  if (is.character(block_to_predict)) {
-    train_idx = match(block_to_predict, names(rgcca_res$call$blocks))
-    test_idx  = match(block_to_predict, names(X))
+  ### Check that response is among both training and test blocks
+  if (is.character(response)) {
+    train_idx = match(response, names(rgcca_res$call$blocks))
+    test_idx  = match(response, names(blocks_test))
   }
   else {
-    train_idx = match(block_to_predict, 1:length(rgcca_res$call$blocks))
-    test_idx  = match(block_to_predict, 1:length(X))
+    train_idx = match(response, 1:length(rgcca_res$call$blocks))
+    test_idx  = match(response, 1:length(blocks_test))
   }
   if (is.na(train_idx) || is.na(test_idx))
     stop_rgcca(paste0("The block to predict is not among both train and ",
                       "test blocks. Please provide an appropriate one."))
-  if (names(X)[[test_idx]] != names(rgcca_res$call$blocks)[[train_idx]])
+  if (names(blocks_test)[[test_idx]] != names(rgcca_res$call$blocks)[[train_idx]])
     stop_rgcca(paste0("Block to predict was provided as an integer but ",
                       "associated block names do not match between train and ",
                       "test blocks. Reorder your blocks or use a name."))
 
   ### Get train and test target
   y_train = rgcca_res$call$raw[[train_idx]]
-  y_test  = X[[test_idx]]
+  y_test  = blocks_test[[test_idx]]
 
   if ((is.null(dim(y_test)) && !all(dim(y_train)[-1] == 1)) ||
       any(dim(y_test)[-1] != dim(y_train)[-1])
@@ -99,150 +95,101 @@ rgcca_predict = function(
     if (mode(rgcca_res$call$raw[[response]]) == "character") {
       if (length(unique(rgcca_res$call$raw[[response]])) == 1) {
         stop("Only one level in the variable to predict")}
-      X[[test_idx]] = asDisjonctive(X[[test_idx]], levs = unique(rgcca_res$call$raw[[response]]))
+      blocks_test[[test_idx]] = asDisjonctive(blocks_test[[test_idx]], levs = unique(rgcca_res$call$raw[[response]]))
     }
   }
 
   ### Get projected train and test data
-  pred    = rgcca_transform(rgcca_res, X, X_scaled)
-  X_train = rgcca_res$Y[names(pred)]
-  X_train = reformat_projection(X_train[-test_idx])
-  X_test  = reformat_projection(pred[-test_idx])
+  blocks_test_scaled = !(rgcca_res$call$scale)
+  projection         = rgcca_transform(rgcca_res, blocks_test, blocks_test_scaled)
+  X_train            = rgcca_res$Y[names(projection)]
+  X_train            = reformat_projection(X_train[-test_idx])
+  X_test             = reformat_projection(projection[-test_idx])
 
   # Keep same lines in X_train and y_train
   y_train = subset_rows(y_train, rownames(X_train))
 
   ### Train prediction model and predict results on X_test
-  # TODO: Work on this part
-  prediction = NULL
-  res        = NULL
-  if (task == "regression")
-  {
+  core_prediction = function(model_info, X_train, X_test, y_train, y_test){
+    data = as.data.frame(cbind(X_train, obs = unname(y_train)))
+    model = train(obs   ~ .,
+                  data      = data,
+                  method    = method,
+                  trControl = trainControl(method = "none"), ...)
 
-    if (prediction_model == "lm")
-    {
-
-      #  if(regress_on=="block")
-      #  {
-      ychapo <- sapply(
-        colnames(y_train),
-        function(x) {
-          predict(
-            lm(
-              as.formula(paste(x, " ~ ",paste(colnames(X_train),collapse="+"))),
-              data = cbind(X_train, y_train),
-              na.action = "na.exclude"
-            ),
-            cbind(X_test, y_test))
-        })
-      if (any(is.na(ychapo)))
-        warning("NA in predictions.")
-
-      # f defined but never used
-      f <- quote(
-        if (is.null(dim(y)))
-          y[x]
-        else
-          y[, x]
-      )
-
-      prediction=ychapo
-      res=data.matrix(y_test - ychapo)
-
-      if (is.null(dim(res))||dim(res)[1]==1) {
-        rmse=sqrt(mean(res^2,na.rm=T))
-        score <- rmse
-      } else{
-        rmse<- apply(res,2,function(x){return(sqrt(mean(x^2,na.rm=T)))})
-        score <- mean(rmse)
-        #score <- mean(apply(res, 2, mean))
+    prediction_train = data.frame(obs  = unname(y_train),
+                                  pred = predict(model, X_train))
+    prediction_test  = data.frame(obs  = unname(y_test),
+                                  pred = predict(model, X_test))
+    if ((model_info$forClass) && (!is.numeric(y_train))){
+      prediction_train$obs = as.factor(prediction_train$obs)
+      prediction_test$obs  = as.factor(prediction_test$obs)
+      confusion_train      = confusionMatrix(prediction_train$pred,
+                                             reference = prediction_train$obs)
+      confusion_test       = confusionMatrix(prediction_test$pred,
+                                             reference = prediction_test$obs)
+      if (model_info$probModel){
+        prediction_train = data.frame(cbind(prediction_train,
+                                            predict(model, X_train, type = "prob")))
+        prediction_test  = data.frame(cbind(prediction_test,
+                                            predict(model, X_test, type = "prob")))
       }
-
-      #   }
-      #   if(regress_on=="comp")
-      #   {
-      #TODO
-      #   }
+      metric_train     = multiClassSummary(data = prediction_train,
+                                           lev  = levels(prediction_train$obs))
+      metric_test      = multiClassSummary(data = prediction_test,
+                                           lev  = levels(prediction_test$obs))
     }
-    if(prediction_model=="cor")
-    {
-      # X_test.cor <- get_comp_all(rgcca_res, X=newA, type = "test", pred = pred)
-      X_test.cor <- get_comp_all(rgcca_res, X = X, type = "test", pred = pred)
 
-      if (is.null(X[[1]])) {
-        # TODO ??? check case for vector
-        X_test
-      } else{
-        connection <- rgcca_res$call$connection[names(pred), names(pred)]
-        # cor <- get_cor_all(rgcca_res, newA, X_test)
-        # cor <- get_cor_all(rgcca_res, newA3, X_test.cor)
-        cor <- get_cor_all(rgcca_res, X, X_test.cor)
-
-        for (i in seq(length(cor))) {
-          cor[[i]] <- mean(
-            abs(cor[[i]] * connection)[upper.tri(connection)],
-            na.rm = TRUE
-          )
-        }
-
-        score <- mean(unlist(cor), na.rm = TRUE)
-      }
+    if ((model_info$forReg) && (is.numeric(y_train))){
+      confusion_train = confusion_test = NA
+      metric_train    = postResample(pred = prediction_train$pred,
+                                     obs  = prediction_train$obs)
+      metric_test     = postResample(pred = prediction_test$pred,
+                                     obs  = prediction_test$obs)
     }
-    class.fit <- NULL
-  }
-  if (task == "classification")
-  {
-    # ngroups defined but never used
-    ngroups   <- nlevels(as.factor(y_train))
-    class.fit <- switch(prediction_model,
-                        "lda"      = {
-                          data_for_lda=cbind(X_train,y_train)
-                          colnames(data_for_lda)[ncol(data_for_lda)]="quali"
-                          reslda     <- lda(quali~., data=data_for_lda, na.action = "na.exclude")
-                          class.fit  <- predict(reslda, X_test)$class
-                        }#,
-                        #             # "logistic" = {
-                        #             #     if (ngroups > 2) {
-                        #             #         reslog      <- nnet::multinom(y ~ ., data = cbind(X_train, y = y_train), trace = FALSE, na.action = "na.exclude")
-                        #             #         class.fit   <- predict(reslog, newdata = cbind(X_test, y = y_test))
-                        #             #     } else if (ngroups == 2) {
-                        #             #         levs=levels(factor(y_train))
-                        #             #         y_train=factor(y_train,levels=levs)
-                        #             #         y_test=factor(y_test,levels=levs)
-                        #             #         data_for_lda=cbind(X_train,y_train)
-                        #             #         colnames(data_for_lda)[ncol(data_for_lda)]="quali"
-                        #             #         reslog      <- glm(y ~ ., data = cbind(X_train, y = y_train), family = binomial,na.action="na.exclude")
-                        #             #         class.fit   <- predict(reslog, type = "response", newdata = cbind(X_test, y = y_test))
-                        #             #         class.fit.class <- class.fit > 0.5 # TODO: cutoff parameter
-                        #             #         class.fit       <- factor(class.fit.class)
-                        #             #     }
-                        #             # }
+    return(list(model      = model,
+                prediction = list(train = prediction_train, test = prediction_test),
+                metric     = list(train = metric_train, test = metric_test),
+                confusion  = list(train = confusion_train, test = confusion_test))
     )
-
-
-    if(length(class.fit)==1)
-    {
-      res=class.fit==as.vector(y_test)
-      score=1-res
-    }
-    else
-    {
-      res=class.fit ==y_test
-      score <- 1-(sum(res) / length(y_test))
-    }
-
-
   }
 
-  result=list(
-    pred = pred,
-    #    pred_A=pred_A,
-    prediction=prediction,
-    class.fit = class.fit,
-    score = score,
-    res = res,
-    rgcca_res=rgcca_res
-  )
+  if (method != "cor"){
+    model_info = all_methods[which(all_methods$model == method), ]
+    if ((dim(y_train)[2] >= 2) && (sum(!apply(y_train, 2, is.numeric)) != 0)){
+      stop_rgcca(paste0("Multiple columns prediction is not handled."))
+    }else{
+      results = mapply(function(x, y) core_prediction(model_info, X_train, X_test, x, y),
+                       as.data.frame(y_train),
+                       as.data.frame(y_test))
+    }
+  }else if(method == "cor"){
+    X_train_all = reformat_projection(rgcca_res$Y[names(projection)])
+    X_test_all  = reformat_projection(projection)
+    connection  = rgcca_res$call$connection[names(projection), names(projection)]
+    connection[connection == 0] = NA
+    cor_train   = get_cor_all(rgcca_res = rgcca_res, comps = X_train_all)
+    cor_test    = get_cor_all(rgcca_res = rgcca_res, blocks = blocks_test,
+                              comps = X_test_all)
+
+    for (i in seq(length(cor))) {
+      cor_train[[i]] = mean(abs(cor_train[[i]] * connection)[upper.tri(connection)],
+                            na.rm = TRUE)
+      cor_test[[i]]  = mean(abs(cor_test[[i]] * connection)[upper.tri(connection)],
+                            na.rm = TRUE)
+    }
+    metric_train = mean(unlist(cor_train), na.rm = TRUE)
+    metric_test  = mean(unlist(cor_test), na.rm = TRUE)
+
+    results           = matrix(list(NA), 4, 1)
+    results[3, 1]     = list(list(train = metric_train, test = metric_test))
+    rownames(results) = c("model", "prediction", "metric", "confusion")
+  }
+
+  result=list(projection = projection,
+              results    = results,
+              rgcca_res  = rgcca_res
+              )
 
   class(result)="predict"
   return(result)
