1a3291043f0ab39b65065d95f61f01ca75049242
Arnaud <arnaud.gloaguen@supelec.fr>
2021-09-29 09:59:35 +0200

    Documentation improved.

--- R/remove_null_sd.R
+++ R/remove_null_sd.R
@@ -1,7 +1,18 @@
-# Remove column having a standard deviation equals to 0
-#
-# @param list_m A list of dataframe
-# @return A list of dataframe
+#' Remove column having a standard deviation equals to 0
+#'
+#' @param list_m A list of dataframe
+#' @param column_sd_null Either NULL or a list of named vectors. If NULL, the
+#' function will search for variables with null variance in each block. If not
+#' NULL, this list defines for each block the index of the variables that are
+#' of null variance (see the 'Value' section for more details about the content
+#' of this list). In both cases, these variables are removed.
+#' @return \item{list_m}{A list of dataframe.}
+#' @return \item{column_sd_null}{Either NULL, if not a single variable was removed,
+#' or a list of the same size as the number of blocks. In the last situation, each
+#' element of this list if again NULL is not a single variable was removed from
+#' the current block, or a named vector indicating the former index of the removed
+#' variables along with their name.}
+#' @keywords internal
 
 remove_null_sd <- function(list_m, column_sd_null = NULL) {
 
