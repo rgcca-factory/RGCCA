09bbe18185c6cf7b84a533b854e45a2c6aa3f7d9
GFabien <fabien.girka@supelec.fr>
2022-03-08 17:18:59 +0100

    Run styler on all files

--- tests/testthat/test_is.character2.r
+++ tests/testthat/test_is.character2.r
@@ -2,13 +2,11 @@
 
 #'''
 x <- matrix(c(runif(10), LETTERS[seq(10)]), 10, 2)
-is.character2(x)
-# TRUE
-is.character2(LETTERS[seq(10)])
-# TRUE
-test_that("is.character2", {
+test_that("is.character2 returns TRUE if x contains at least a character", {
   expect_true(is.character2(LETTERS[seq(10)]))
-})
-test_that("is.character2", {
   expect_true(is.character2(x))
+})
+test_that("is.character2 returns FALSE if x does not contain any character", {
+  expect_false(is.character2(1:10))
+  expect_false(is.character2(c(rnorm(5), "NA", NA)))
 })
