edd302d814a3f1fd17da0a4cbe2f2be3c8af8532
GFabien <fabien.girka@supelec.fr>
2022-03-24 15:20:37 +0100

    Use caret to make prediction in rgcca_predict

--- tests/testthat/test_rgcca_predict.r
+++ tests/testthat/test_rgcca_predict.r
@@ -4,7 +4,7 @@ data("Russett")
 blocks <- list(
   agriculture = Russett[, 1:3],
   industry = Russett[, 4:5],
-  politic = Russett[, 6:11]
+  politic = Russett[, 6:8]
 )
 
 C <- connection <- matrix(c(
@@ -21,9 +21,9 @@ A <- lapply(blocks, function(x) x[1:32, ])
 A <- lapply(blocks, function(x) x[1:32, ])
 object1 <- rgcca(A,
   connection = C, tau = c(0.7, 0.8, 0.7),
-  ncomp = c(3, 2, 4), superblock = FALSE, response = 3
+  ncomp = c(3, 2, 3), superblock = FALSE, response = 3
 )
-res <- rgcca_predict(rgcca_res = object1, X = A, X_scaled = FALSE, block_to_predict = "politic")
+res <- rgcca_predict(rgcca_res = object1, blocks_test = A, response = "politic")
 reslm_1_res <- apply(object1$call$raw$politic, 2, function(x) {
   lm(x ~ object1$Y[[1]][, 1:3] + object1$Y[[2]][, 1:2])$residuals
 })
@@ -46,14 +46,14 @@ object3 <- rgcca(A3,
   ncomp = c(1, 1, 1), superblock = FALSE, response = 1
 )
 
-res <- rgcca_predict(object3, newA3, X_scaled = FALSE, block_to_predict = "agriculture")
+res <- rgcca_predict(object3, newA3, response = "agriculture")
 reslm_1_res <- apply(object3$call$raw$agriculture, 2, function(x) {
   lm(x ~ object3$Y[[2]][, 1] + object3$Y[[3]][, 1])$residuals
 })
 
-test_that("rgcca_predict_rmse", {
+test_that("rgcca_predict_residuals", {
   expect_true(
-    sum(round(res$res, digits = 5) - round(reslm_1_res, digits = 5)) == 0
+    sum(round(A3[[1]] - res$prediction, digits = 5) - round(reslm_1_res, digits = 5)) == 0
   )
 })
 
@@ -75,28 +75,13 @@ object4 <- rgcca(A4,
   connection = C, tau = c(0.7, 0.8, 0.7),
   ncomp = c(1, 1, 1), superblock = FALSE, response = 1
 )
-res <- rgcca_predict(object4, newA4, X_scaled = FALSE, block_to_predict = "agriculture")
-test_that("rgcca_predict_rmse3", {
+res <- rgcca_predict(object4, newA4, response = "agriculture")
+
+test_that("rgcca_predict_residuals", {
   expect_true(
-    sum(round(res$res, digits = 5) == round(reslm_1_res[1, ], digits = 5)) == 3
+    sum(round(A4[[1]][1, ] - res$prediction, digits = 5) == round(reslm_1_res[1, ], digits = 5)) == 3
   )
 })
-
-
-
-# cor
-# A = lapply(blocks, function(x) x[1:32,]);
-# A_test=lapply(blocks,function(x) x[c(39:47),])
-#                ncomp = c(3,2,4), superblock = FALSE, response = 3)
-
-# res_test  = rgcca_predict(object1, A_test,X_scaled=FALSE,prediction_model="cor",block_to_predict="politic")
-# test_that("rgcca_predict_two",{expect_true(
-#    sum(!abs(res_test$pred[[1]]["Sweden",]- object1$Y[[1]]["Sweden",])<1e-12)==0
-# )})
-
-
-
-
 
 
 #-----------------------
@@ -109,14 +94,14 @@ RussettWithNA[3, 1] <- NA
 blocksNA <- list(
   agriculture = RussettWithNA[, seq(3)],
   industry = RussettWithNA[, 4:5],
-  politic = RussettWithNA[, 6:11]
+  politic = RussettWithNA[, 6:8]
 )
 A_test <- lapply(blocksNA, function(x) x[c(39:47), ])
 object1 <- rgcca(A,
   connection = C, tau = c(0.7, 0.8, 0.7),
   ncomp = c(1, 1, 1), superblock = FALSE, response = 3
 )
-res_test <- rgcca_predict(object1, A_test, X_scaled = FALSE, block_to_predict = "politic")
+res_test <- rgcca_predict(object1, A_test, response = "politic")
 
 # classfication and LDA
 #---------------------------------
@@ -135,23 +120,10 @@ object1 <- rgcca(A,
   connection = C, tau = c(1, 1, 1),
   ncomp = c(3, 2, 1), superblock = FALSE, response = 3
 )
-res_test <- rgcca_predict(object1, X = A, X_scaled = FALSE, prediction_model = "lda", task = "classification", block_to_predict = "politic")
+res_test <- rgcca_predict(object1, blocks_test = A, prediction_model = "lda", response = "politic")
 
-#   res_test  = rgcca_predict(object1, A_test,X_scaled=FALSE,prediction_model="lda",task="classification",block_to_predict="politic")
 test_that("rgcca_predict_classif", {
   expect_true(
     res_test$score == 1 - 0.875
   )
 })
-#
-
-
-# A = lapply(blocks_for_classif, function(x) x[1:40,]);
-#   object1 = rgcca(A, connection = C, tau = c(1,1,1),
-#                 ncomp = c(3,2,1), superblock = FALSE, response = 3)
-# res_test  = rgcca_predict(object1, X=A,X_scaled=FALSE,prediction_model="logistic",task="classification",block_to_predict="politic")
-
-#   res_test  = rgcca_predict(object1, A_test,X_scaled=FALSE,prediction_model="lda",task="classification",block_to_predict="politic")
-# test_that("rgcca_predict_classif",{expect_true(
-#    res_test$score==0.875
-# )})
