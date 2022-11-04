fd451fedabf433423d38f18b1d0a521e742e9ff1
GFabien <fabien.girka@supelec.fr>
2022-11-04 10:23:51 +0100

    Improve plot.bootstrap test coverage

--- tests/testthat/test_plot.bootstrap.R
+++ tests/testthat/test_plot.bootstrap.R
@@ -10,6 +10,20 @@ blocks <- list(
 fit.rgcca <- rgcca(blocks, ncomp = 2, method = "rgcca", tau = 1)
 fit.boot <- bootstrap(fit.rgcca, n_boot = 20, n_cores = 1, verbose = FALSE)
 
+test_that("plot.bootstrap raises an error if block is not \"all\" or an integer
+          between 1 and the number of blocks", {
+  expect_error(
+    plot.bootstrap(fit.boot, block = "toto"),
+    "block must be equal to \"all\" or an integer between 1 and 3.",
+    fixed = TRUE
+  )
+  expect_error(
+    plot.bootstrap(fit.boot, block = 5),
+    "block must be equal to \"all\" or an integer between 1 and 3.",
+    fixed = TRUE
+  )
+})
+
 test_that("plot.bootstrap", {
   vdiffr::expect_doppelganger(
     "Bootstrap weight", plot.bootstrap(fit.boot, type = "weight")
@@ -17,7 +31,7 @@ test_that("plot.bootstrap", {
 
   vdiffr::expect_doppelganger(
     "Bootstrap loadings", plot.bootstrap(
-      fit.boot, type = "loadings", show_sign = FALSE
+      fit.boot, type = "loadings", show_sign = FALSE, block = 1
     )
   )
 })
