871e545e4674d55ec099e6fb31250e7edd81b6de
GFabien <fabien.girka@supelec.fr>
2022-11-21 14:53:03 +0100

    Fix AVE computation with missing values

--- R/rsq.R
+++ R/rsq.R
@@ -2,12 +2,12 @@
 #' @noRd
 rsq <- function(y, X) {
   if (NCOL(y) == 1) {
-    return(mean(cor(X, y) ^ 2))
+    return(mean(cor(X, y, use = "pairwise.complete.obs") ^ 2))
   }
   reg <- suppressWarnings(lm(X ~ y))
   mean(
     1 - apply(
       matrix(reg$residuals, ncol = NCOL(reg$residuals)), 2, function(x) sum(x^2)
-    ) / apply(X, 2, function(x) sum(x^2))
+    ) / apply(X, 2, function(x) sum(x^2, na.rm = TRUE))
   )
 }
