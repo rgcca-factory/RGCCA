65110248ffd52d384a9511e4b3b0f8a6f2652317
GFabien <fabien.girka@supelec.fr>
2023-01-04 16:09:15 +0100

    Make format_bootstrap_list much faster

--- R/format_bootstrap_list.R
+++ R/format_bootstrap_list.R
@@ -6,24 +6,27 @@
 #' The aim of this function is to reorganize this list into a data.frame.
 #' @param W raw bootstrap results
 #' @return A data.frame containing the results.
-#' @importFrom tidyr pivot_longer
 #' @noRd
+
 format_bootstrap_list <- function(W, rgcca_res) {
-  ### Combine data.frames for each bootstrap sample, weights and loadings, and
-  ### each block.
-  Reduce(rbind, lapply(seq_along(W), function(b) {
-    Reduce(rbind, lapply(seq_along(W[[b]]), function(i) {
-      Reduce(rbind, lapply(seq_along(W[[b]][[i]]), function(j) {
-        # For each block, construct a data.frame
-        block <- W[[b]][[i]][[j]]
-        colnames(block) <- seq_len(NCOL(block))
-        df <- data.frame(var = rownames(block), block) %>%
-          pivot_longer(!.data$var, names_to = "comp", names_prefix = "X")
-        df$block <- names(W[[b]][[i]])[j]
-        df$type <- ifelse(i == 1, "weights", "loadings")
-        df$boot <- b
-        return(df)
-      }))
-    }))
+  # Repeat values for variables, blocks and components
+  grid <- do.call(rbind, lapply(seq_along(rgcca_res$a), function(j) {
+    a <- rgcca_res$a[[j]]
+    expand.grid(list(
+      var = rownames(a),
+      comp = seq_len(NCOL(a)),
+      block = names(rgcca_res$a)[j]
+    ), stringsAsFactors = FALSE)
   }))
+
+  # Repeat values by adding the type and the number of the bootstrap sample
+  grid <- merge(grid, expand.grid(list(
+    type = c("weights", "loadings"),
+    boot = seq_along(W)
+  ), stringsAsFactors = FALSE), by = NULL)
+
+  # Unlist the values into a new column of the grid
+  grid$value <- unlist(W, use.names = FALSE)
+
+  return(grid)
 }
