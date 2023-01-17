5d8bcda24d7f266ecbe3ade9804ae469573df67c
Arnaud <arnaud.gloaguen@gmail.com>
2023-01-17 12:51:11 +0100

    Forbid a_k to be set to a_1 as function is not define in a_1.

--- R/proj_l1_l2.R
+++ R/proj_l1_l2.R
@@ -11,7 +11,7 @@
 #' @importFrom stats median
 #' @noRd
 proj_l1_l2 <- function(x, a = 1) {
-
+  
   # Check if constraints are already satisfied
   norm2_x <- norm(x, type = "2")
   if (norm2_x < .Machine$double.eps) {
@@ -20,37 +20,40 @@ proj_l1_l2 <- function(x, a = 1) {
   if (sum(abs(x / norm2_x)) <= a) {
     return(list(k = NaN, lambda = 0, l2_sat = TRUE))
   }
-
+  
   # The desired a_k cannot be null as the constraints are not already satisfied
   # (cf. previous check). So zero values are removed.
   uneq <- x != 0
   p <- abs(x[uneq])
-
+  
   # Check for multiple maximum
   MAX <- max(p)
+  # 1e-13 is the gap tolerance to define a maximum. Otherwise, in some
+  # cases, there could be numerical problems (indeed the function psi is 
+  # not defined for MAX = a_1).
   bMAX <- (abs(p - MAX) <= 1e-13)
   nMAX <- sum(bMAX)
-
+  
   # If there are multiple maximum, the sparse parameter
   # "a" must be >= sqrt(number of max)
   if (a < sqrt(nMAX)) {
     sol <- x * 0
-    idx_MAX_val <- which(abs(x) == MAX)
+    idx_MAX_val <- which(bMAX)
     sol[idx_MAX_val] <- sign(x[idx_MAX_val]) * a / nMAX
     return(list(sol = sol, l2_sat = FALSE))
   }
-
+  
   # If there are multiple maximum and a = sqrt(number of max),
   # solution is straightforward
   if (a == sqrt(nMAX)) {
     if (nMAX == length(p)) {
-
+      
       # Case where there is as many maximum as the number of elements of the
       # vector to project "x". Indeed in the case,
       # ||x||_1/||x||_2 = sqrt(number of max)
       lambda <- 0
     } else {
-
+      
       # With this choice of lambda, the soft-thresholding will set all
       # parameters to 0 except for the elements equals to the maximum value.
       a_2 <- max(p[-which(bMAX)])
@@ -58,7 +61,7 @@ proj_l1_l2 <- function(x, a = 1) {
     }
     return(list(k = NaN, lambda = lambda, l2_sat = TRUE))
   }
-
+  
   # If the vector to project "x" is composed of 2 elements only, as the
   # sparse parameter a <= 1 the desired a_k is a_2 (the lowest element) as
   # psi(a_2) = 1 (cf. theory) and by construction a_3 = 0 and psi(0) >= a
@@ -72,52 +75,66 @@ proj_l1_l2 <- function(x, a = 1) {
       (sum(p) - k * a_k) / (psi_a_k * (k))
     return(list(k = NaN, lambda = lambda, l2_sat = TRUE))
   }
-
+  
   # Initialize parameters
   s_1 <- s_2 <- nb <- 0
   repeat {
-    N <- length(p)
+    # the psi function is not defines for a_1 (the highest element of x in 
+    # absolute value) so it is removed for the step where a "a_k" is selected.
+    # Nonetheless it must be kept in "p"!! 
+    if (MAX %in% p){
+      bMAX <- (abs(p - MAX) <= 1e-13)
+      p_MAX_rmv <- p[!bMAX]
+    } else {
+      p_MAX_rmv <- p
+    }
+    # after removing all the maximums, there is still at least one element
+    # as the case where all elements are maximums have been dealt with earlier.
+    # If by any chance, a_2 is selected, them p_low is composed only of the 
+    # maximum and the next step would result in N being equal to zero. However, 
+    # is a_2 is selected, psi(a_2)=1<= s, so p_high is necessarily selected.
+    N <- length(p_MAX_rmv)
     if (N == 0) {
-      warning("length(p) = 0")
+      warning("length(p_MAX_rmv) = 0")
       break
     }
-
+    
     # Choose next a_k
     if (N %% 2 == 0) {
-
-      # remove max instead of min -> psi not defined for max(abs(x))
-      p_reduced <- p[-which.max(p)]
-
+      
+      # Either min or max is possible 
+      p_reduced <- p_MAX_rmv[-which.max(p_MAX_rmv)]
+      
       # Replace by ccaPP::fastMedian or any homemade cpp median function
       a_k <- median(p_reduced)
     } else {
-
+      
       # Replace by ccaPP::fastMedian or any homemade cpp median function
-      a_k <- median(p)
+      a_k <- median(p_MAX_rmv)
     }
-
+    
     # Make a partition of list p
     p_inf_ak <- p < a_k
     p_sup_ak <- p > a_k
     p_high <- p[p_inf_ak]
     p_low <- p[p_sup_ak]
-
+    
     # Evaluation decreasing rank of a_k
     nb_a_k <- sum(p == a_k)
     k <- nb + sum(p_sup_ak) + nb_a_k
-
+    
     # Compute value of the constraint
     aksq <- a_k^2
     s_low_1 <- sum(p_low) + nb_a_k * a_k
-
+    
     # NOTE : could create  : ssq   <- function(u) sum(u**2) -> not necessary
     # could use norm(u, type = "2")^2 -> not working when u = as.numeric(0)
     # (mean p_low is empty)
     # When p_low is empty, sum(p_low**2) = 0, which is what is wanted.
     s_low_2 <- sum(p_low**2) + nb_a_k * aksq
     psi_a_k <- (s_1 + s_low_1 - k * a_k) /
-      sqrt(max(s_2 + s_low_2 - 2 * a_k * (s_1 + s_low_1) + k * aksq, 1e-16))
-
+      sqrt(s_2 + s_low_2 - 2 * a_k * (s_1 + s_low_1) + k * aksq)
+    
     # Choose partition depending on the constraint
     if (psi_a_k > a) {
       if (length(p_low) == 0) break
@@ -139,7 +156,7 @@ proj_l1_l2 <- function(x, a = 1) {
       }
     }
   }
-
+  
   # Compute lambda
   lambda <- a_k - (a * sqrt((k - psi_a_k^2) / (k - a^2)) - psi_a_k) *
     (s_1 + s_low_1 - k * a_k) / (psi_a_k * (k))
