a28a9d8f5f71986164ae2a531f7204c19b1eceb3
Arnaud <arnaud.gloaguen@gmail.com>
2023-01-31 19:20:19 +0100

    Minor adjustments

--- R/proj_l1_l2.R
+++ R/proj_l1_l2.R
@@ -24,29 +24,29 @@ proj_l1_l2 <- function(x, a = 1) {
   # The desired a_k cannot be null as the constraints are not already satisfied
   # (cf. previous check). So zero values are removed.
   uneq <- x != 0
-  p <- abs(x[uneq])
+  p0 <- abs(x[uneq])
   
   # Check for multiple maximum
-  MAX <- max(p)
+  MAX0 <- max(p0)
   # 1e-13 is the gap tolerance to define a maximum. Otherwise, in some
   # cases, there could be numerical problems (indeed the function psi is 
-  # not defined for MAX = a_1).
-  bMAX <- (abs(p - MAX) <= 1e-13)
-  nMAX <- sum(bMAX)
+  # not defined for MAX0 = a_1).
+  bMAX0 <- (abs(p0 - MAX0) <= 1e-13)
+  nMAX0 <- sum(bMAX0)
   
   # If there are multiple maximum, the sparse parameter
   # "a" must be >= sqrt(number of max)
-  if (a < sqrt(nMAX)) {
+  if (a < sqrt(nMAX0)) {
     sol <- x * 0
-    idx_MAX_val <- which(bMAX)
-    sol[idx_MAX_val] <- sign(x[idx_MAX_val]) * a / nMAX
+    idx_MAX_val0 <- which(bMAX0)
+    sol[idx_MAX_val0] <- sign(x[idx_MAX_val0]) * a / nMAX0
     return(list(sol = sol, l2_sat = FALSE))
   }
   
   # If there are multiple maximum and a = sqrt(number of max),
   # solution is straightforward
-  if (a == sqrt(nMAX)) {
-    if (nMAX == length(p)) {
+  if (a == sqrt(nMAX0)) {
+    if (nMAX0 == length(p0)) {
       
       # Case where there is as many maximum as the number of elements of the
       # vector to project "x". Indeed in the case,
@@ -56,8 +56,8 @@ proj_l1_l2 <- function(x, a = 1) {
       
       # With this choice of lambda, the soft-thresholding will set all
       # parameters to 0 except for the elements equals to the maximum value.
-      a_2 <- max(p[-which(bMAX)])
-      lambda <- (MAX + a_2) / 2
+      a_2 <- max(p0[-which(bMAX0)])
+      lambda <- (MAX0 + a_2) / 2
     }
     return(list(k = NaN, lambda = lambda, l2_sat = TRUE))
   }
@@ -67,32 +67,30 @@ proj_l1_l2 <- function(x, a = 1) {
   # psi(a_2) = 1 (cf. theory) and by construction a_3 = 0 and psi(0) >= a
   # (checked previously). Moreover, these 2 elements are necessarily different
   # (cf. conditions above)
-  if (length(p) == 2) {
-    a_k <- min(p)
+  if (length(p0) == 2) {
+    a_k <- min(p0)
     psi_a_k <- 1
     k <- 2
     lambda <- a_k - (a * sqrt((k - psi_a_k^2) / (k - a^2)) - psi_a_k) *
-      (sum(p) - k * a_k) / (psi_a_k * (k))
+      (sum(p0) - k * a_k) / (psi_a_k * (k))
     return(list(k = NaN, lambda = lambda, l2_sat = TRUE))
   }
   
   # Initialize parameters
   s_1 <- s_2 <- nb <- 0
+  p <- p0
   repeat {
     # the psi function is not defines for a_1 (the highest element of x in 
     # absolute value) so it is removed for the step where a "a_k" is selected.
     # Nonetheless it must be kept in "p"!! 
-    if (MAX %in% p){
-      bMAX <- (abs(p - MAX) <= 1e-13)
-      p_MAX_rmv <- p[!bMAX]
-    } else {
-      p_MAX_rmv <- p
-    }
+    bMAX <- (abs(p - MAX0) <= 1e-13)
+    p_MAX_rmv <- p[!bMAX]
     # after removing all the maximums, there is still at least one element
     # as the case where all elements are maximums have been dealt with earlier.
     # If by any chance, a_2 is selected, them p_low is composed only of the 
     # maximum and the next step would result in N being equal to zero. However, 
-    # is a_2 is selected, psi(a_2)=1<= s, so p_high is necessarily selected.
+    # is a_2 is selected, psi(a_2)=sqrt(nMax)<= s, so p_high is necessarily 
+    # selected.
     N <- length(p_MAX_rmv)
     if (N == 0) {
       warning("length(p_MAX_rmv) = 0")
