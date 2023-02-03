4d945aedc828347e4aefb056276acc71d261129f
GFabien <fabien.girka@supelec.fr>
2023-02-03 17:18:21 +0100

    Add documentation about the AVE computation

--- R/ave.R
+++ R/ave.R
@@ -1,6 +1,13 @@
 #' Compute the AVE (Average Variance Explained) for each component for
 #' a given block.
 #'
+#' For PCA, the AVE of a component y associated to a block X is
+#' ||Cov(X, y)||_2^2 / (Tr(Cov(X)) * Var(y)).
+#' Since ||Cov(X, y)||_2^2 = Sum_{j = 1}^p Cov(X_j, y)^2, the AVE can be written
+#' diag(Cov(X)) %*% Cor(X, y)^2 / Tr(Cov(X)).
+#' The same formula is used for other methods. If components Y are not
+#' orthogonal, a QR decomposition is sequentially applied and Y is replaced
+#' by Q in the correlation.
 #' @param X A matrix containing a given block.
 #' @param Y A matrix containing the components associated to the given block.
 #' @importFrom stats var
