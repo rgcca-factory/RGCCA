964044bd219eb87d140bf5786068c6d66dab3f0d
Elen Goujon <goujon.elen@hotmail.fr>
2025-03-05 09:49:45 +0100

    Fixed omission in gradient

--- R/block_update.R
+++ R/block_update.R
@@ -16,7 +16,7 @@ block_update.dual_block <- function(x, grad) {
 
 #' @export
 block_update.ac_block <- function(x, grad) {
-  x$f <- pm(x$f_left, grad, na.rm = x$na.rm) -
+  x$f <- pm(x$f_left, 1/x$N * grad, na.rm = x$na.rm) -
     pm(x$f_right, x$a, na.rm = x$na.rm)
   return(block_project(x))
 }
