Tenenhaus/Tenenhaus

Add first version of simultaneous RGCCA

R/block_init.R
@@ -39,3 +39,14 @@ block_init.dual_regularized_block <- function(x, init = "svd") {
   x$M <- ginv(x$tau * diag(x$n) + (1 - x$tau) * x$K / x$N)
   NextMethod()
 }
+
+#' @export
+block_init.sim_block <- function(x, init = "svd") {
+  if (init == "svd") {
+    x$a <- initsvd(x$x, dual = FALSE, ncomp = x$ncomp)
+  } else {
+    x$a <- matrix(rnorm(x$p * x$ncomp), nrow = x$p)
+  }
+
+  return(block_project(x))
+}

rgcca-factory/RGCCA

R/block_init.R
@@ -41,6 +41,17 @@ block_init.dual_regularized_block <- function(x, init = "svd") {
 }
 
 #' @export
+block_init.sim_block <- function(x, init = "svd") {
+  if (init == "svd") {
+    x$a <- initsvd(x$x, dual = FALSE, rank = x$ncomp)
+  } else {
+    x$a <- matrix(rnorm(x$p * x$ncomp), nrow = x$p)
+  }
+
+  return(block_project(x))
+}
+
+#' @export
 block_init.tensor_block <- function(x, init = "svd") {
   if (init == "svd") {
     x$factors <- lapply(seq_along(dim(x$x))[-1], function(m) {