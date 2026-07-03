579d0b5e568ced7c8235372e6afa679d36c22619
sofiavarib <sofiavarib@gmail.com>
2026-07-03 10:00:23 +0200

    fix summary for bootstrap and description

--- R/summary.rgcca_bootstrap.r
+++ R/summary.rgcca_bootstrap.r
@@ -9,7 +9,7 @@ summary.rgcca_bootstrap <- function(object,
   ### Perform checks and parse arguments
   stopifnot(is(object, "rgcca_bootstrap"))
   type <- type[1]
-  type <- match.arg(type, c("weights", "loadings"))
+  type <- match.arg(type, c("weights", "loadings","factors"))
   lapply(block, function(i) check_blockx("block", i, object$rgcca$call$blocks))
   Map(function(y, z) check_compx(y, y, object$rgcca$call$ncomp, z), comp, block)
 
@@ -24,20 +24,41 @@ summary.rgcca_bootstrap <- function(object,
       "th_upper_bound", "bootstrap_ratio", "th_pval", "adjust.pval"
     )
   }
-  df <- object$stats[object$stats$type == type, ]
+ # df <- object$stats[object$stats$type == type, ]
+  multi_blocks <- sapply(object$rgcca$call$blocks, function(b) {
+    is.array(b) && length(dim(b)) > 2
+  })
+  
+  blocks_names <- names(object$rgcca$blocks)
+  
+  df_list <- lapply(seq_along(block), function(ii) {
+    j <- block[ii]  
+    bn <- blocks_names[j]
+    use_type <- if (multi_blocks[j]) "factors" else type
+    comp_j <- if (length(comp) == 1) comp else comp[ii]
+    subset(object$stats, type == use_type & block == bn & comp == comp_j)
+  })
+  
+  df <- do.call(rbind, df_list)
   col_pval <- ifelse(empirical, "pval", "th_pval")
+  if (col_pval %in% colnames(df)) {
+    df[, col_pval] <- p.adjust(df[, col_pval], method = adj.method)
+  } else {
+    df[, col_pval] <- NA
+  }
   df["adjust.pval"] <- p.adjust(df[, col_pval], method = adj.method)
-  df <- df[df$block %in% names(object$rgcca$blocks)[block], ]
-  df <- df[df$comp == comp, ]
+ # df <- df[df$block %in% names(object$rgcca$blocks)[block], ]
+ # df <- df[df$comp == comp, ]
   rownames(df) <- df$var
   df <- df[, columns]
   colnames(df) <- column_names
 
   if (display_order) {
     df <- df[order(abs(df$estimate), decreasing = TRUE), ]
-  } else {
-    df <- df[unlist(lapply(object$rgcca$blocks[block], colnames)), ]
-  }
+
+  }# else {
+    #df <- df[unlist(lapply(object$rgcca$blocks[block], colnames)), ]
+  #}
 
   df <- format(df, digits = 3)
 
